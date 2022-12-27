6ff708c7316ee4c34cd79ba0bb51998cac4a33b4
sirish12389 <sirish12389@gmail.com>
2022-12-27 16:41:23 +0530

    Added JobPostRepo

---
+++ JobPostLibrary/IJobPostRepoAsync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPostLibrary
+{
+    public interface IJobPostRepo
+    {
+        Task<List<JobPost>> GetAllJobsAsync();
+        Task<JobPost> GetJobsByPostId(int postId);
+        void InsertIntoJobPost(JobPost post);
+        void UpdateIntoJobPost(int postId, JobPost post);
+        void DeleteFromJobPost(int postId);
+    }
+}
